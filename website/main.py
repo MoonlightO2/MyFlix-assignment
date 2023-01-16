649a285921d66b3ed0074d7a96e3b41d3e54161f
Shashini Peiris <114579042+MoonlightO2@users.noreply.github.com>
2023-01-16 10:43:30 +0000

    Update main.py

--- website/main.py
+++ website/main.py
@@ -1,27 +1,38 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
-from werkzeug.utils import secure_filename
 import os
-from wtforms.validators import InputRequired
+from flask import Flask, render_template, redirect, flash, request, send_from_directory
+from werkzeug.utils import secure_filename
+from utils import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['UPLOADS_FOLDER'] = '/media/pics'
+app.config['SECRET_KEY'] = 'my secret'
 
-class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
-    submit = SubmitField("Upload File")
+@app.route('/', methods=["GET", "POST"])
+def index():
+  if request.method == "GET":
+    return render_template('index.html')
+  
+  if not 'file' in request.files:
+    flash('No file part in request')
+    return redirect(request.url)
 
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
-def home():
-    form = UploadFileForm()
-    if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return "File has been uploaded."
-    return render_template('index.html', form=form)
+  files = request.files.getlist('file')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+  for file in files:
+    if file.filename == '':
+      flash('No file uploaded')
+      return redirect(request.url)
+
+    if file_valid(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOADS_FOLDER'], filename))
+    else:
+      flash('Invalid file type')
+      return redirect(request.url) 
+      
+  return "Files uploaded successfully"
+
+@app.route('/media/pics/<path:filename>')
+def send_attachment(filename):
+  return send_from_directory(app.config['UPLOADS_FOLDER'], 
+    filename=filename, as_attachment=True)
